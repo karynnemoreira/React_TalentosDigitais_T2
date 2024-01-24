5584e1a998ed32fc7200f760ddf53d982717616b
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-01-24 19:47:40 -0300

    primeira aula de react t2

---
+++ primeira-aula-t2/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Boa tarde!!</h1>
+      <h2>Sou o conteúdo principal</h2>
+      <h3>Toda vez que eu retornar mais de uma tagm eu preciso de uma tag envolvendo</h3>
+    </>
+
+  )
+}
+
+export default App
